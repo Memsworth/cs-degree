b767bc0ebb48d33a95ca320edd9892e4064916de
Abubakar Ahmed <memsworth.97@posteo.com>
2026-05-10 23:58:47 +0530

    leve13

---
+++ semester1/csharp-players-guide/src/level13/ChallengeCountdown.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpPlayersGuide.level13;
+
+internal class ChallengeCountdown : IExercise
+{
+    public void Run()
+    {
+        Kaboom(10);
+    }
+
+
+
+    public void Kaboom(int num)
+    {
+        if (num == 0)
+            Console.WriteLine("Kaboom!");
+        else
+        {
+            Console.WriteLine($"{num}");
+            Kaboom(num - 1);
+        }
+    }
+}
---
+++ semester1/csharp-players-guide/src/level13/ChallengeTakingNumber.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpPlayersGuide.level13;
+
+internal class ChallengeTakingNumber : IExercise
+{
+    public void Run()
+    {
+        var age = AskForNumber("Enter age");
+        var age2 = AskForNumberInRange("Enter age2", 10, 15);
+    }
+
+    private int AskForNumber(string text)
+    {
+        var result = 0;
+        while(true)
+        {
+            Console.WriteLine($"{text} ");
+            if (!int.TryParse(Console.ReadLine(), out result))
+                break;
+        }
+        return result;
+    }
+
+    private int AskForNumberInRange(string text, int min, int max)
+    {
+        var result = 0;
+        while (true)
+        {
+            Console.WriteLine($"{text} ");
+            if (!int.TryParse(Console.ReadLine(), out result))
+                if (result > min && result < max)
+                    break;
+        }
+        return result;
+    }
+}
