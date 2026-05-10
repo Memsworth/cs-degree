108b10d49c66ca212adbd4eef56ea48b4d707705
Abubakar Ahmed <memsworth.97@posteo.com>
2026-05-11 00:47:15 +0530

    BossChallenge-level14

--- semester1/csharp-players-guide/Program.cs
+++ semester1/csharp-players-guide/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpPlayersGuide;
 using CSharpPlayersGuide.Level11;
+using CSharpPlayersGuide.level14;
 
-IExercise exercise = new ChallengeMagicCannon();
+IExercise exercise = new ChallengeHuntingManticore();
 exercise.Run();
